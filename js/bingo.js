7a144428782256191f228857f789ed15d84cf66c
Butchie1331 <butchie1331jp@gmail.com>
2022-06-05 20:20:02 +0900

    add startSlots

--- js/bingo.js
+++ js/bingo.js
@@ -16,15 +16,53 @@ var bingo = function (size) {
 	var TYPE = gup('type');
 	var SIZE = gup('size') ? gup('size') : size;
 	var EXPLORATION = gup("exploration");
+	var START = gup("start");
+
+	var slots = [];
+	var defaultSlots = [];
+	var tlbrSlots = [];
+	var bltrSlots = [];
 
 	if (SIZE == 3) {
 		$('#size3').prop('checked', true);
+		slots = [1, 2, 3, 6, 7, 8, 11, 12, 13];
+		defaultSlots = [1, 13];
+		tlbrSlots = [1, 7, 13];
+		bltrSlots = [3, 7, 11];
 	} else if (SIZE == 4) {
 		$('#size4').prop('checked', true);
+		slots = [1, 2, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14, 16, 17, 18, 19];
+		defaultSlots = [7, 13];
+		tlbrSlots = [1, 7, 13, 19];
+		bltrSlots = [4, 8, 12, 16];
+	} else {
+		$('#size5').prop('checked', true);
+		slots = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+		defaultSlots = [7, 19];
+		tlbrSlots = [1, 7, 13, 19, 25];
+		bltrSlots = [5, 9, 13, 17, 21];
 	}
 
-	if (EXPLORATION) {
+	var startSlots = [];
+	var valid = false;
+	startSlots = START.split(",").map((str) => parseInt(str));
+	startSlots.forEach((num) => {
+		if (slots.includes(num)) {
+			valid = true;
+		}
+	})
+	if (!valid) {
+		startSlots = defaultSlots;
+	}
+	slots.forEach((slot) => {
+		if (startSlots.includes(slot)) {
+			$("#init-slot" + slot).text("●");
+		}
+	})
+
+	if (EXPLORATION == 1) {
 		$('#exploration-check').prop('checked', true);
+		$("#exploration-init").show();
 	}
 
 	if (SEED == "") return reseedPage(TYPE);
@@ -41,7 +79,7 @@ var bingo = function (size) {
 	results.append("<p>HK Bingo JP <strong>v1</strong>&emsp;Seed: <strong>" +
 		SEED + "</strong>&emsp;Card type: <strong>" + cardtype + "</strong></p>");
 
-	if (!EXPLORATION) {
+	if (EXPLORATION != 1) {
 		$('.popout').click(function () {
 			var type = null;
 			var line = $(this).attr('id');
@@ -87,56 +125,41 @@ var bingo = function (size) {
 		}
 	);
 
+	$("#exploration-init-table tr").on('click', 'td',
+		function () {
+			if ($(this).text() == "●") {
+				var slot = parseInt($(this).attr('id').slice(9));
+				for (i = 1; i <= 25; i++) {
+					if ((i != slot) && ($("#init-slot" + i).text() == "●")) {
+						$(this).text("");
+					}
+				}
+			} else {
+				$(this).text("●");
+			}
+		}
+	);
+
 	if (SIZE == 4) {
 		$("#row5").hide();
 		$("#col5").hide();
-		$("#slot5").hide();
-		$("#slot10").hide();
-		$("#slot15").hide();
-		$("#slot20").hide();
-		$("#slot21").hide();
-		$("#slot22").hide();
-		$("#slot23").hide();
-		$("#slot24").hide();
-		$("#slot25").hide();
-		$("#slot5").removeClass("bltr");
-		$("#slot9").removeClass("bltr");
-		$("#slot13").removeClass("bltr");
-		$("#slot17").removeClass("bltr");
-		$("#slot21").removeClass("bltr");
-		$("#slot4").addClass("bltr");
-		$("#slot8").addClass("bltr");
-		$("#slot12").addClass("bltr");
-		$("#slot16").addClass("bltr");
 	} else if (SIZE == 3) {
 		$("#row4").hide();
 		$("#row5").hide();
 		$("#col4").hide();
 		$("#col5").hide();
-		$("#slot4").hide();
-		$("#slot5").hide();
-		$("#slot9").hide();
-		$("#slot10").hide();
-		$("#slot14").hide();
-		$("#slot15").hide();
-		$("#slot16").hide();
-		$("#slot17").hide();
-		$("#slot18").hide();
-		$("#slot19").hide();
-		$("#slot20").hide();
-		$("#slot21").hide();
-		$("#slot22").hide();
-		$("#slot23").hide();
-		$("#slot24").hide();
-		$("#slot25").hide();
-		$("#slot5").removeClass("bltr");
-		$("#slot9").removeClass("bltr");
-		$("#slot13").removeClass("bltr");
-		$("#slot17").removeClass("bltr");
-		$("#slot21").removeClass("bltr");
-		$("#slot3").addClass("bltr");
-		$("#slot7").addClass("bltr");
-		$("#slot11").addClass("bltr");
+	}
+	for (i = 1; i <= 25; i++) {
+		if (!slots.includes(i)) {
+			$("#slot" + i).hide();
+			$("#init-slot" + i).hide();
+		}
+		if (tlbrSlots.includes(i)) {
+			$("#slot" + i).addClass("tlbr");
+		}
+		if (bltrSlots.includes(i)) {
+			$("#slot" + i).addClass("bltr");
+		}
 	}
 
 	$("#row1").hover(function () { $(".row1").addClass("hover"); }, function () { $(".row1").removeClass("hover"); });
@@ -167,25 +190,15 @@ var bingo = function (size) {
 		//populate the actual table on the page
 		for (i = 1; i <= 25; i++) {
 			$('#slot' + i).append(bingoBoard[i - 1].name);
-			if (SIZE == 3) {
-				if (EXPLORATION && i != 1 && i != 13) {
-					$('#slot' + i).addClass('hidden');
-				}
-			} else if (SIZE == 4) {
-				if (EXPLORATION && i != 7 && i != 13) {
-					$('#slot' + i).addClass('hidden');
-				}
-			} else {
-				if (EXPLORATION && i != 7 && i != 19) {
-					$('#slot' + i).addClass('hidden');
-				}
+			if (EXPLORATION == 1 && !startSlots.includes(i)) {
+				$('#slot' + i).addClass('hidden');
 			}
 			//$('#slot'+i).append("<br/>" + bingoBoard[i].types.toString());
 			//$('#slot'+i).append("<br/>" + bingoBoard[i].synergy);
 		}
 
 		var bingosync_goals = JSON.stringify(bingoBoard);
-		if (EXPLORATION) {
+		if (EXPLORATION == 1) {
 			$('#bingosync-goals').text("Explorationモードが有効のため非表示です。Copyは可能です。");
 			$('#bingosync-goals-hidden').text(bingosync_goals);
 		} else {
@@ -206,8 +219,59 @@ function reseedPage(type) {
 	var qType = (type == "short" || type == "long") ? "&type=" + type : "";
 	var qSize = $('#size3').is(':checked') ? "&size=3" : $('#size4').is(':checked') ? "&size=4" : "";
 	var qEx = $('#exploration-check').is(':checked') ? '&exploration=1' : '';
-	window.location = qSeed + qType + qSize + qEx;
+	var qStart = "";
+
+	if (qEx != "") {
+		var startSlots = [];
+		for (i = 1; i <= 25; i++) {
+			if ($("#init-slot" + i).text() == "●") {
+				startSlots.push(i);
+			}
+		}
+		qStart = "&start=" + startSlots.join(",");
+	}
+
+	window.location = qSeed + qType + qSize + qEx + qStart;
 	return false;
+}
+
+function changeSizeRadio(size) {
+	var slots = [];
+	var defaultSlots = [];
+
+	if (size == 3) {
+		slots = [1, 2, 3, 6, 7, 8, 11, 12, 13];
+		defaultSlots = [1, 13];
+	} else if (size == 4) {
+		slots = [1, 2, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14, 16, 17, 18, 19];
+		defaultSlots = [7, 13];
+	} else {
+		slots = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+		defaultSlots = [7, 19];
+	}
+
+	for (i = 1; i <= 25; i++) {
+		if (slots.includes(i)) {
+			$("#init-slot" + i).show();
+		} else {
+			$("#init-slot" + i).hide();
+		}
+
+		if (defaultSlots.includes(i)) {
+			$("#init-slot" + i).text("●");
+		} else {
+			$("#init-slot" + i).text("");
+		}
+	}
+
+}
+
+function changeExCheck() {
+	if ($('#exploration-check').is(':checked')) {
+		$("#exploration-init").show();
+	} else {
+		$("#exploration-init").hide();
+	}
 }
 
 function copyJson() {
@@ -216,12 +280,12 @@ function copyJson() {
 	if (navigator.clipboard) {
 		navigator.clipboard.writeText(json).then(() => {
 			$('#copyButton').text("✔");
-			$('#copyButton').css("pointer-events","none")
-			$('#copyButton').css("cursor","default")
+			$('#copyButton').css("pointer-events", "none")
+			$('#copyButton').css("cursor", "default")
 			setTimeout(function () {
 				$('#copyButton').text("Copy")
-				$('#copyButton').css("pointer-events","")
-				$('#copyButton').css("cursor","pointer")
+				$('#copyButton').css("pointer-events", "")
+				$('#copyButton').css("cursor", "pointer")
 			}, 1500);
 		})
 	} else {
